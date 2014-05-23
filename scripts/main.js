5011a811f4b59d5c194ea886e1fb1377c5d18f59
Matt McFadyen <mmcfadyen91@gmail.com>
2014-05-23 14:10:59 -0400

    cycling through each members quotes

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
 var app = {
 
-	quotes: [null,null,null,null], 
+	//array of quotes of leaders.
+	quotes: [0,0,0,0], 
 
 	leaders : ['stephen-harper','justin-trudeau','thomas-mulcair','elizabeth-may'],
 	mps: {},
 
 
-	search_limit : '5',
+	search_limit : 5,
 
-	current_id : 0,
-	//next bubble
-	bubble_id : 0,
+	current_id : 0, //which leader
+	quote_id : 0,  //which quote
 
 	main_url: 'http://api.openparliament.ca',
 
@@ -24,40 +24,14 @@ var app = {
 			success: function(response){
 				//store the mentioned politicians quotes
 				app.quotes[id] = response; 
+				
 				//set current politician
 				app.current_id = id;
-
-				app.bubble_id = 0;
-
+				//reset quote
+				app.quote_id = 0;
 
 				app.nextQuote();
-
-
-
-
-
-				// //where text is being applied
-				// var div = $('.bubble');
-
-				// for (var i = 0; i < app.search_limit; i ++) {
-				// 	//what they said
-				// 	var content = (response.objects[i].content.en);
-
-				// 	//URL of politician who said it.
-				// 	var attribution = (response.objects[i].politician_url);
-
-				// 	//stats of the politician who said it 
-				// 	//second AJAX call
-				// 	stats = app.buildAttribution(attribution,content,id,i);
-
-				// 	//set speach bubble	
-				// 	div.html(content);
-
-				// 	//shitSaid[id] = contentRecord(content,)
-				// }
-
-				
-				
+					
 			}
 		});//ajax
 	},//query
@@ -82,79 +56,51 @@ var app = {
 				console.log('setting MP key: ' +mpURL);
 				app.mps[mpURL] = response; 
 
-
 				var speaker = $('.speaker');
 				speaker.find('img').attr('src',app.main_url+app.mps[mpURL].image);
 				speaker.find('#full-name').text(app.mps[mpURL].name);
 				speaker.find('#party-riding').text(app.mps[mpURL].memberships[0].party.name.en);
-
-				//var div = $('.speaker');
-				
-				// console.log(content);
-				// var img = app.main_url+response.image;
-				// div.find('img').attr('src',app.main_url+response.image);
-				
-				// var name = response.name;
-				// div.find('#full-name').text(response.name);
-
-				// var partyRiding = response.memberships[0].party.name.en;
-				// div.find('#party-riding').text(response.memberships[0].party.name.en);
-
-				//app.contentRecord(content,img,name,partyRiding,id,i);
-
 
 			}
 
 		});//ajax
 	},//buildAttribution
 
-
+	//Displays the content and speaker information
+	//increases iteration for the bubble.
 	nextQuote: function(){
 		var bubble  = $('.bubble');
 		var speaker = $('.speaker');
-		console.log('quotes: ' +app.quotes[app.current_id].objects[app.bubble_id].content.en);
-		bubble.html(app.quotes[app.current_id].objects[app.bubble_id].content.en);
+		
+		//set content bubble.
+		bubble.html(app.quotes[app.current_id].objects[app.quote_id].content.en);
 
 		//get quotes for specific leader
 		var quotes = app.quotes[app.current_id];
 		//get mp's URL of who said current quote;
-		var mpURL = quotes.objects[app.bubble_id].politician_url;
+		var mpURL = quotes.objects[app.quote_id].politician_url;
 		
 		//if the MP isn't cached
 		if (app.mps[mpURL] === undefined){
 			app.buildAttribution(mpURL);
 		}
-		//get cache and set the dude.
-		else {
-			// var index = '\'' + mpURL + '\'';
-			// console.log(index);
-			console.log(app.main_url+app.mps[mpURL].image);			
+		//get cache and set the speaker.
+		else {		
 			speaker.find('img').attr('src',app.main_url+app.mps[mpURL].image);
 			speaker.find('#full-name').text(app.mps[mpURL].name);
 			speaker.find('#party-riding').text(app.mps[mpURL].memberships[0].party.name.en);
 		}
-		
 
+		app.quote_id++;
+		console.log('incrementing quote_id: ' + app.quote_id);
 
-
-
+		if (app.quote_id === app.search_limit){
+			app.quote_id = 0;
+			console.log('reseting quote_id: ' + app.quote_id);
+		}
 
 
 	},//nextQuote
-
-
-	//create a record of who mentioned a leader
-	//@param content - what they said about mentioned leader
-	//@param img - image of the politician who said this
-	//@param name - name of the politician who said this
-	//@param partyRiding - Party and Riding of the politician who saids.
-	//@param id - refers to which politician is being mentioned
-	//@param i - refers to which index in the leaders array
-	contentRecord: function(content,img,name,partyRiding,id,i){
-		var record = { content: content, image: img, fullName: name, party: partyRiding};
-		
-		console.log(app.shitSaid);
-	},
 
 
 	//change all a tags to include api.openparliment.ca
@@ -172,22 +118,65 @@ var app = {
 
 $(document).ready(function(){
 
+	//clicking on a Leader's name.
 	$('li').on('click',function(){
-		//each li has an id corresponding to the
+		//each li has an id corresponding to the leaders array
 		var id = Number(this.id);
 		app.current_id = id;
 		//if the Leader hasn't already been queried
-		if(app.quotes[id] !== undefined){
+		if(app.quotes[id] === 0){
+			console.log("making a query for: " + app.leaders[id]);
 			//query the leader
 			app.query(app.leaders[id],id);
 		}
 		else {
+			console.log("retrieving cache for: " + app.leaders[id]);
 			app.nextQuote();
 			//recall the cache
 		}	
 
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
